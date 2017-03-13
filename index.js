4d419abdf7c2bb7de0be3038bf7143bde1534d1c
David Chase <david.chase4@gmail.com>
2017-03-13 16:33:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 function dwarfRollCall(dwarves) {
+  var arr = []
+
+  for (let i = 0, l = dwarves.length; i < l; i++) {
+    arr.push(`${i + 1}. ${dwarves[i]} `)
+  }
+
+  return arr.join('')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = []
+
+  for (let i = 0, l = planeteerCalls.length; i < l; i++) {
+    arr.push(`${planeteerCalls[i].toUpperCase}!`)
+  }
+  return arr
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0, l = words.length; i < l; i++) {
+    if (words[i].length > 4) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  const cheeses = ['cheddar', 'gouda', 'camembert']
+
+  for (let i = 0, l = cheeses.length; i < l; i++) {
+    for (let j = 0, m = foods.length; j < m; j++) {
+      if (cheeses[i] === foods[j]) {
+        return foods[j]
+      }
+    }
+  }
+  return 'no cheese!'
 }
